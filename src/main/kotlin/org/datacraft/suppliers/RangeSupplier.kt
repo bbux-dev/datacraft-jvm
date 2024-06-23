567decd4fdedd07274da96699125de99605cfc41
Brian Buxton <bbux.dev@gmail.com>
2024-06-23 16:39:06 -0700

    Fixes for range supplier, added step param

--- src/main/kotlin/org/datacraft/suppliers/RangeSupplier.kt
+++ src/main/kotlin/org/datacraft/suppliers/RangeSupplier.kt
@@ -4,13 +4,14 @@ import org.datacraft.ValueSupplier
 
 class RangeSupplier(
     private val start: Int,
-    private val end: Int
+    private val end: Int,
+    private val step: Int
 ) : ValueSupplier<Any> {
     private var current = start
 
     override fun next(iteration: Long): Any {
         val result = current
-        current++
+        current+=step
         if (current > end) {
             current = start
         }
--- src/test/kotlin/org/datacraft/suppliers/RangeSupplierTest.kt
+++ src/test/kotlin/org/datacraft/suppliers/RangeSupplierTest.kt
@@ -1,15 +1,27 @@
 package org.datacraft.suppliers
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class RangeSupplierTest : StringSpec({
     "range supplier rotates through values in sequence" {
-        val supplier = RangeSupplier(1, 3)
-        supplier.next(0) shouldBe 1
-        supplier.next(1) shouldBe 2
-        supplier.next(2) shouldBe 3
-        supplier.next(3) shouldBe 1
+        val supplier = RangeSupplier(1, 3, 1)
+        assertSoftly {
+            supplier.next(0) shouldBe 1
+            supplier.next(1) shouldBe 2
+            supplier.next(2) shouldBe 3
+            supplier.next(3) shouldBe 1
+        }
+    }
+    "range supplier rotates through values in sequence with step" {
+        val supplier = RangeSupplier(0, 5, 2)
+        assertSoftly {
+            supplier.next(0) shouldBe 0
+            supplier.next(1) shouldBe 2
+            supplier.next(2) shouldBe 4
+            supplier.next(3) shouldBe 0
+        }
     }
 })
